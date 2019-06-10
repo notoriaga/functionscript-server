63631736db39142843bb95ba7b9a2604871ddd59
notoriaga <steven@stdlib.com>
2019-06-09 23:56:45 -0700

    Adds some env vars

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const { Gateway, FunctionParser } = require('functionscript');
 function start() {
   let gateway = new Gateway({
     port: process.env.PORT || 8170,
-    maxRequestSizeMB: process.env.MAX_REQUEST_SIZE,
+    maxRequestSizeMB: process.env.MAX_REQUEST_SIZE && parseInt(process.env.MAX_REQUEST_SIZE),
+    debug: process.env.DEBUG === 'true'
   });
   let functionParser = new FunctionParser();
 
@@ -16,8 +17,6 @@ function start() {
   }
 
   gateway.listen();
-
-  console.log(`Server started on port ${gateway.port}`);
 }
 
 start();
